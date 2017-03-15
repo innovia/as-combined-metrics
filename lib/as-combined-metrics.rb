01d0ee140feb149e12fa4f653567f2c46d4ed01b
Ariel Steiner <asteiner@glidetalk.com>
2017-03-15 13:39:24 +0200

    version 1.1.0

--- lib/as-combined-metrics.rb
+++ lib/as-combined-metrics.rb
@@ -1,5 +1,5 @@
 module AsCombinedMetrics
-  VERSION   = "1.0.6"
+  VERSION   = "1.1.0"
   ABOUT     = "as-combined-metrics v#{VERSION} (c) #{Time.now.strftime("2015-%Y")} @innovia"
 
   $:.unshift File.expand_path(File.join(File.dirname(__FILE__), %w[.. lib]))
